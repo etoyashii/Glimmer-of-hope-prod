b0842f8747d1ced3e06687938c98b9f2e72b7831
Nodeions <ehamida@gaming.tech>
2026-05-20 16:39:15 +0200

    [ADD] Basic Note Combo

---
+++ Assets/_Project/Scripts/Gameplay/Characters/SkillNoteManager.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlimmerOfHope.Gameplay
+{
+    public class SkillNoteManager : MonoBehaviour
+    {
+        #region Enums
+
+        public enum Steps
+        {
+            First,
+            Second,
+            Third
+        }
+
+        //I pref to attach _currentStep around his enum instead of putting on the PrivateFields region, can be moved as you wish
+        private Steps _currentStep = Steps.First;
+
+        #endregion
+
+        #region SerializeFields
+
+        [SerializeField] private GameObject _cubePrefab;
+        [SerializeField] private Transform _spawnPoint;
+
+        #endregion
+
+        #region PrivateFields
+
+        private List<int> _inputNoteList;
+        private int _noteNumber = 3;
+
+        #endregion
+
+        #region UnityLifecycle
+
+        private void Start()
+        {
+            _inputNoteList = new();
+
+            for (int i = 0; i < _noteNumber; i++)
+            {
+                _inputNoteList.Add(-1); //default value
+            }
+            
+            Debug.Log(_inputNoteList.Count);
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public void ActivateNote(int noteIndex)
+        {
+            switch (_currentStep)
+            {
+                case Steps.First:
+                    SaveNote((int)_currentStep, noteIndex);
+                    IncrementStep();
+                    break;
+                case Steps.Second:
+                    SaveNote((int)_currentStep, noteIndex);
+                    IncrementStep();
+                    break;
+                case Steps.Third:
+                    SaveNote((int)_currentStep, noteIndex);
+                    CheckNoteCombo();
+                    ResetStep();
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region PrivateMethods
+
+        private void CheckNoteCombo()
+        {
+            for (int i = 0; i < _inputNoteList.Count; i++)
+            {
+                Debug.Log(_inputNoteList[i].ToString());
+            }
+
+            if (_inputNoteList[0] == 0 && _inputNoteList[1] == 0 && _inputNoteList[2] == 0)
+            {
+                Instantiate(_cubePrefab, _spawnPoint.position, _spawnPoint.rotation);
+            }
+            Debug.Log("Combo checked");
+        }
+
+        private void SaveNote(int currentStep, int noteIndex)
+        {
+            _inputNoteList[currentStep] = noteIndex;
+        }
+
+        private void IncrementStep()
+        {
+            _currentStep++;
+            Debug.Log("Current step : " + _currentStep);
+        }
+
+        private void ResetStep()
+        {
+            _currentStep = Steps.First;
+            Debug.Log("Step resetted");
+        }
+
+        #endregion
+    }
+}
