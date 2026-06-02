e4f570c297c06d41f0c2e1c5c3153343021adc6b
Nodeions <ehamida@gaming.tech>
2026-06-02 16:01:30 +0200

    [UPDATE] Smooth switch light
    Add Coroutine and SmoothDamp

--- Assets/_Project/Scripts/Gameplay/Characters/Enlighten.cs
+++ Assets/_Project/Scripts/Gameplay/Characters/Enlighten.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace GlimmerOfHope.Gameplay
@@ -14,6 +15,7 @@ namespace GlimmerOfHope.Gameplay
         [SerializeField] private float _minIntensity = 0.0f;
         [Range(0.0f, 20.0f)]
         [SerializeField] private float _maxIntensity = 12.0f;
+        [SerializeField] private float _transitionDelay = 1.0f;
 
         #endregion
 
@@ -21,12 +23,38 @@ namespace GlimmerOfHope.Gameplay
 
         public void IncreaseLight()
         {
-            _light.intensity = _maxIntensity;
+            StartCoroutine(SwitchLight(_transitionDelay, true));
         }
 
         public void ReduceLight()
         {
-            _light.intensity = _minIntensity;
+            StartCoroutine(SwitchLight(_transitionDelay, false));
+        }
+
+        #endregion
+
+        #region Coroutines
+
+        IEnumerator SwitchLight(float delay, bool isIncreased)
+        {
+            float startIntensity = _light.intensity;
+            float elapsedTime = 0f;
+            float targetIntensity;
+
+            if (isIncreased)
+                targetIntensity = _maxIntensity;
+            else
+                targetIntensity = _minIntensity;
+
+            while (elapsedTime < delay)
+            {
+                _light.intensity = Mathf.SmoothStep(startIntensity, targetIntensity, elapsedTime / delay);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _light.intensity = targetIntensity;
+
         }
 
         #endregion
