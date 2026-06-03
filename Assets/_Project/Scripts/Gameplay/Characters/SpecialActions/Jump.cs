f64c1ad796ddd2229ccca25439bde7d16fb7433c
Mellias-dev <bmotte@gaming.tech>
2026-06-03 10:30:57 +0200

    [UPDATE] Jump
    incorporation de l'Impulse au Jump en fonction de la grandeur du float injecté dans PerformJump()

--- Assets/_Project/Scripts/Gameplay/Characters/SpecialActions/Jump.cs
+++ Assets/_Project/Scripts/Gameplay/Characters/SpecialActions/Jump.cs
@@ -16,14 +16,35 @@ namespace GlimmerOfHope.Gameplay
         #region SerializedField
         [SerializeField] private CharacterController _controller;
         [SerializeField] private Movement _playerMovement;
-        [SerializeField] private float _jumpForce;
+        [SerializeField] private float _jumpImpulseLimit;
+        [SerializeField] private  ParticleSystem _jumpVFX;
+        [SerializeField] private  ParticleSystem _impulseVFX;
         #endregion
 
         #region Public Methods
-        public void PerformJump()
+        public void PerformJump(float jumpForce)
         {
+
             if (!_controller.isGrounded) return;
-            _playerMovement.verticalVelocity += _jumpForce;
+            if (jumpForce < _jumpImpulseLimit)
+            {
+                if (_jumpVFX != null)
+                {
+                    _jumpVFX.transform.position = gameObject.transform.position;
+                    _jumpVFX.transform.rotation = Quaternion.LookRotation(gameObject.transform.up);
+                    _jumpVFX.Play();
+                }
+            }
+            else
+            {
+                if (_impulseVFX != null)
+                {
+                    _impulseVFX.transform.position = gameObject.transform.position;
+                    _impulseVFX.transform.rotation = Quaternion.LookRotation(gameObject.transform.up);
+                    _impulseVFX.Play();
+                }
+            }
+            _playerMovement.verticalVelocity += jumpForce;
         }
         #endregion 
     }
