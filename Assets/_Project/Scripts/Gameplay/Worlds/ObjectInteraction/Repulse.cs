055e35bf8dff68c788655f15e0850fc571c5cc67
Erwan Hamida <147034141+ErwanHAMIDA@users.noreply.github.com>
2026-06-23 13:29:36 +0200

    [UPDATE] Repulse
    using signalManager + attenuation ratio added

--- Assets/_Project/Scripts/Gameplay/Worlds/ObjectInteraction/Repulse.cs
+++ Assets/_Project/Scripts/Gameplay/Worlds/ObjectInteraction/Repulse.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace GlimmerOfHope.Gameplay
@@ -9,14 +10,17 @@ namespace GlimmerOfHope.Gameplay
         [Range(10.0f, 1000.0f)]
         [SerializeField] private float _forceImpulse = 200.0f;
 
+        private float _attenuationRatio = 100.0f;
+
         #endregion
         private void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.CompareTag("Player"))
             {
-                Vector3 newVector = Vector3.back + Vector3.up;
+                Vector3 newVector = Vector3.back + Vector3.up / _attenuationRatio;
 
                 collision.rigidbody.AddForce(newVector * _forceImpulse, ForceMode.Impulse);
+                PlayerSignalManager.GetInstance().SendBlinkSignal();
             }
         }
     }
