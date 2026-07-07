2543255bf6ce981cc8744a9a895529506c04215a
Romain Boone <rboone@gaming.tech>
2026-07-07 14:34:06 +0200

    [UPDATE] Desactivate vfx emotion

--- Assets/_Project/Scripts/Gameplay/Characters/ViewEmotion/VFX_Emotion.cs
+++ Assets/_Project/Scripts/Gameplay/Characters/ViewEmotion/VFX_Emotion.cs
@@ -4,25 +4,42 @@ using UnityEngine.VFX;
 
 namespace GlimmerOfHope.Gameplay
 {
+    /// <summary>
+    /// Script to manage the vfx link to the emotion of a npc
+    /// </summary>
     public class VFX_Emotion : MonoBehaviour
     {
-        [SerializeField] private VisualEffect vfx;
+        [SerializeField] private GlobalValueShader _manager;
+
+        [SerializeField] private VisualEffect _vfx;
         [ColorUsage(showAlpha:true, hdr: true)]
-        [SerializeField] private Color color;
+        [SerializeField] private Color _color;
         [Range(0f, 1f)]
-        [SerializeField] private float amplitude = 0.01f;
-        [SerializeField] private float frequency = 1f;
-        [SerializeField] private float rotationSpeed = 1f;
-        [SerializeField] private float rotationRange = 90f;
+        [SerializeField] private float _amplitude = 0.01f;
+        [SerializeField] private float _frequency = 1f;
+        [SerializeField] private float _rotationSpeed = 1f;
+        [SerializeField] private float _rotationRange = 90f;
 
         // Update is called once per frame
         void Update()
         {
-            vfx.SetVector4("Color", color);
-            vfx.SetFloat("Amplitude", amplitude);
-            vfx.SetFloat("Frequency", frequency);
-            vfx.SetFloat("RotationSpeed", rotationSpeed);
-            vfx.SetFloat("RotationRange", rotationRange);
+            _vfx.SetVector4("Color", _color);
+            _vfx.SetFloat("Amplitude", _amplitude);
+            _vfx.SetFloat("Frequency", _frequency);
+            _vfx.SetFloat("RotationSpeed", _rotationSpeed);
+            _vfx.SetFloat("RotationRange", _rotationRange);
+
+            float distance = Vector3.Distance(transform.position, _manager.playerTransform.position);
+            float currentCircleSize = _manager.currentPropRadius * _manager.radiusBigCircle;
+
+            if (distance <= currentCircleSize && _manager.viewEmotionIsActive)
+            {
+                _vfx.gameObject.SetActive(false);
+            }
+            else
+            {
+                _vfx.gameObject.SetActive(true);
+            }
         }
     }
 }
