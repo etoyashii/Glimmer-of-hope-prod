9f58103e570aa8615b01f25dcd707109f998014b
Neelgc <nbesson@gaming.tech>
2026-08-10 12:18:13 +0200

    Codex system ok

--- Assets/_Project/Scripts/UI/Codex/PartitionSequenceRow.cs
+++ Assets/_Project/Scripts/UI/Codex/PartitionSequenceRow.cs
@@ -1,32 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using GlimmerOfHope.UI.BookMenu.Data;
 
 namespace GlimmerOfHope.UI.BookMenu.Panels
 {
-    public class PartitionInfoRow : MonoBehaviour
-    {
-        #region Private Fields
-
-        [SerializeField] private Image _thumbnail;
-        [SerializeField] private TMP_Text _titleText;
-        [SerializeField] private TMP_Text _descriptionText;
-
-        #endregion
-
-        #region Public Methods
-
-        public void Setup(PartitionData data)
-        {
-            if (_thumbnail != null) _thumbnail.sprite = data.Thumbnail;
-            if (_titleText != null) _titleText.text = data.Title;
-            if (_descriptionText != null) _descriptionText.text = data.Description;
-        }
-
-        #endregion
-    }
-
     public class PartitionSequenceRow : MonoBehaviour
     {
         #region Private Fields
